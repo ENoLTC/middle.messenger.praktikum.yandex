3855445d252e036a5a7fd96a0278391435faf60a
e.novozhilov@docsinbox.ru <docsGfhjkm2106>
2021-04-06 01:25:51 +0300

    EventBus

---
+++ src/services/EventBus.ts
@@ -0,0 +1,39 @@
+interface Listeners {
+  [event: string]: ((args?: any) => void)[];
+}
+
+export class EventBus {
+  listeners: Listeners;
+
+  constructor() {
+    this.listeners = {} as Listeners;
+  }
+
+  on(event: string, callback: () => void) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: () => void) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter(
+      listener => listener !== callback
+    );
+  }
+
+  emit(event: string, ...args: any) {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach(function(listener) {
+      listener(...args);
+    });
+  }
+}
