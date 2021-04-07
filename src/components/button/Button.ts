40de9641a9d77aade7fe8cdd78f85a78c9dec794
e.novozhilov <e.novozhilov@docsinbox.ru>
2021-04-07 22:47:39 +0300

    Button Component

---
+++ src/components/button/Button.ts
@@ -0,0 +1,16 @@
+import Handlebars from 'handlebars';
+import {buttonTemplate} from './Button.handlebars';
+import './Button.scss'
+import {Component} from "../Component";
+
+export class Button extends Component {
+  constructor(props) {
+    super('div', undefined, props);
+  }
+
+  render(): string {
+    console.log(this.props)
+    const template = Handlebars.compile(buttonTemplate);
+    return template({...this.props});
+  }
+}
--- src/components/button/button.ts
+++
@@ -1,5 +0,0 @@
-import Handlebars from 'handlebars';
-import {buttonTemplate} from './button.handlebars';
-import './button.scss'
-
-export const compiledButton = Handlebars.compile(buttonTemplate);
